227bbb24cde77c9d73b254ba2a14c0c059178cd0
dbroms <49290240+dbroms@users.noreply.github.com>
2019-04-05 07:53:12 -0700

    Add files via upload

--- planets.py
+++ planets.py
@@ -1,7 +1,7 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    earthw= int(input('What do you weigh on earth? '))
+    print("On Mars you would weigh", earthw * .38, "pounds.","\n" "On Jupiter you would weigh", earthw * 2.34, "pounds.")
+
+
 if __name__ == '__main__':
    weight_on_planets()
